8cda4f920ab5982a80a51a8b285cd5d3c4005a67
Igor Simakov <i.simakov@banki.ru>
2023-05-30 15:35:20 +0800

    export BaseEntity into jpa module

---
+++ lib/jpa/src/main/java/ru/simakov/BaseEntity.java
@@ -0,0 +1,40 @@
+package ru.simakov;
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Objects;
+
+@MappedSuperclass
+@SuperBuilder
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
