2b07470668d44b9f3a9d2f25161032b6de034417
Igor Simakov <i.simakov@banki.ru>
2023-06-05 14:58:18 +0800

    add flyway migration

--- lib/jpa/src/main/java/ru/simakov/BaseEntity.java
+++ lib/jpa/src/main/java/ru/simakov/BaseEntity.java
@@ -21,7 +21,7 @@ import java.util.Objects;
 @ToString
 public class BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Override
